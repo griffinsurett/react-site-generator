3efe6debdf0e4c177100b72a547796d578c32515
griffinjoshs <leveloutusa@gmail.com>
2025-01-25 17:15:28 -0500

    test to netlify

--- react-site-generator/src/CMS/Headers-Footers/Headers.js
+++ react-site-generator/src/CMS/Headers-Footers/Headers.js
@@ -8,14 +8,14 @@
  * Handles analytics-related elements for the <head> tag.
  */
 
-import {
-    initializeGoogleAnalytics,
-    initializeGoogleTagManager,
-    initializeFacebookPixel,
-    initializeGoogleSearchConsole,
-    // initializeFacebookSDK,
-    initializeGoogleMaps,
-  } from "../Utils/SEO/Analytics";
+// import {
+//     initializeGoogleAnalytics,
+//     initializeGoogleTagManager,
+//     initializeFacebookPixel,
+//     initializeGoogleSearchConsole,
+//     // initializeFacebookSDK,
+//     initializeGoogleMaps,
+//   } from "../Utils/SEO/Analytics";
   
   export const initializeAnalyticsHead = () => {
     // console.log("Initializing analytics and SDKs in <head>...");
